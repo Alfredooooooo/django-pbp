421ad9cb3ba5e7ddb08675d632867386cb264e71
Alfredo <alfredo.austin@ui.ac.id>
2022-09-14 23:53:10 +0700

    Finishing Tutorial 2

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,8 +1,14 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
-
 # Create your views here.
+
+
+def show_XML(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 
 def show_wishlist(request):
@@ -12,3 +18,18 @@ def show_wishlist(request):
         'nama': 'Alfredo Austin'
     }
     return render(request, "wishlist.html", context)
+
+
+def show_JSON(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+def show_JSON_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+def show_XML_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
