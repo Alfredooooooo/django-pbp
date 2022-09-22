dfc0b86ae1bd4376fe32dc06f52287d5b23c20c8
Alfredo <alfredo.austin@ui.ac.id>
2022-09-22 08:50:14 +0700

    Lab 3 First Commit

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from wishlist.views import show_wishlist
-from wishlist.views import show_XML
-from wishlist.views import show_JSON
-from wishlist.views import show_JSON_by_id
-from wishlist.views import show_XML_by_id
+from wishlist.views import register, login_user, logout_user, show_wishlist, show_XML, show_JSON, show_JSON_by_id, show_XML_by_id
 
 app_name = 'wishlist'
 
 urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     path('', show_wishlist, name='show_wishlist'),
     path('xml/', show_XML, name='show_XML'),
     path('json/', show_JSON, name='show_JSON'),
